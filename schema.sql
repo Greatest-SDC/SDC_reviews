ea8e05009f33aab340e0861291e79b79bebd04d2
Adam Whitman <emagdaeh@yahoo.com>
2021-03-17 12:06:21 -0700

    Refactored table pre-debugging with postgres

--- schema.sql
+++ schema.sql
@@ -1,43 +1,42 @@
-DROP TABLE products;
 DROP TABLE reviews;
 DROP TABLE characteristics;
+DROP TABLE characteristic_reviews;
 DROP TABLE reviews_photos;
-
-CREATE TABLE products (
-  id SERIAL PRIMARY KEY,
-  product VARCHAR(6) NOT NULL,
-  ratings JSONB,
-  recommended JSONB
-);
 
 CREATE TABLE reviews (
   id SERIAL PRIMARY KEY,
   product_id INTEGER,
-  FOREIGN KEY (product_id) REFERENCES products (id),
-  review_id INTEGER,
   rating INTEGER,
-  summary VARCHAR(50),
-  recommended BOOLEAN,
-  response VARCHAR(1000),
-  body VARCHAR(1000),
   date TIMESTAMP,
+  summary VARCHAR(50),
+  body VARCHAR(1000),
+  recommended BOOLEAN,
+  reported BOOLEAN,
   reviewer_name VARCHAR(25),
+  reviewer_email VARCHAR(35),
+  response VARCHAR(1000),
   helpfulness INTEGER
 );
 
 CREATE TABLE characteristics (
   id SERIAL PRIMARY KEY,
   product_id INTEGER,
-  FOREIGN KEY (product_id) REFERENCES products (id),
-  characteristics_name VARCHAR(7),
-  characteristics_id INTEGER,
-  value VARCHAR(18)
+  FOREIGN KEY (product_id) REFERENCES reviews (product_id),
+  name VARCHAR(7)
+);
+
+CREATE TABLE characteristic_reviews (
+  id SERIAL PRIMARY KEY,
+  characteristic_id INTEGER,
+  FOREIGN KEY (characteristic_id) REFERENCES characteristics (id),
+  review_id INTEGER,
+  FOREIGN KEY (review_id) REFERENCES reviews (id),
+  value VARCHAR(1)
 );
 
 CREATE TABLE reviews_photos (
   id SERIAL PRIMARY KEY,
   review_id INTEGER,
   FOREIGN KEY (review_id) REFERENCES reviews (id),
-  photo_id INTEGER,
   url VARCHAR(100)
 );
