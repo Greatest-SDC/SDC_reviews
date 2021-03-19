366bd6eaa1d0e5f2031f8328453c11ada6a53392
Adam Whitman <emagdaeh@yahoo.com>
2021-03-18 23:47:32 -0700

    Most of the nest objects are built for meta, still need to tie them together and clean up characteristics obj

--- server/server.js
+++ server/server.js
@@ -18,7 +18,8 @@ const reviewsResultsArrayBuilder = (num) => {
   database.query(
     `SELECT reviews.review_id, reviews.rating, reviews.summary, reviews.recommend, reviews.response, reviews.body, reviews.date, reviews.reviewer_name, reviews.helpfulness, (
       SELECT JSON_BUILD_OBJECT(
-        'id', id, 'url', url
+        'id', id,
+        'url', url
       ) AS photos
       FROM reviews_photos
       WHERE reviews_photos.review_id = ${num}
@@ -45,38 +46,41 @@ app.get('/reviews', (req, res) => {
 
 //============================================================
 
-// This builds the ratings object contents without the key of "ratings"
-// `SELECT
-//   SUM(1) AS "1",
-//   SUM(2) AS "2",
-//   SUM(3) AS "3",
-//   SUM(4) AS "4",
-//   SUM(5) AS "5"
-// FROM reviews
-// WHERE product = ${num}
-
-// This builds the recommended object contents without the key of "recommended"
-// SELECT
-//   COUNT(*) filter (WHERE NOT "recommend") AS false,
-//   COUNT(*) filter (WHERE "recommend") AS true
-// FROM reviews
-// WHERE product = ${num}
-
+// This builds the ratings object contents with the key of "ratings", but the values are integers not strings
 // SELECT JSON_BUILD_OBJECT(
-//   'photos', (SELECT JSON_AGG(ROW_TO_JSON("reviews_photos")) FROM reviews_photos))
+//   '1', SUM(1),
+//   '2', SUM(2),
+//   '3', SUM(3),
+//   '4', SUM(4),
+//   '5', SUM(5)
+// ) AS ratings
+// FROM reviews
+// WHERE product = ${num}
 
-// Still need to compile tables to get characteristics name with appropriate id and value
+// This builds the recommended object contents, but the values of false and true are integers, not strings
+// SELECT JSON_BUILD_OBJECT(
+//   'false', COUNT(*) filter (WHERE NOT "recommend"),
+//   'true', COUNT(*) filter (WHERE "recommend")
+// ) AS recommended
+// FROM reviews
+// WHERE product = ${num}
+
+// This builds an object with the names, ids, and values of the characteristics tied to the product id
+// SELECT
+//   characteristics.name, characteristics.id, characteristic_reviews.value
+// FROM characteristics, characteristic_reviews
+// WHERE characteristics.product_id = ${num} AND characteristic_reviews.characteristic_id = characteristics.id
 
 // This creates average value for characteristics.quality.value
 // `SELECT AVG(value) AS "value" FROM characteristic_reviews`
 
-
 const metadataObjectBuilder = (num) => {
 
   database.query(
-    `SELECT
-      COUNT(*) filter (WHERE NOT "recommend") AS false,
-      COUNT(*) filter (WHERE "recommend") AS true
+    `SELECT JSON_BUILD_OBJECT(
+      'false', COUNT(*) filter (WHERE NOT "recommend"),
+      'true', COUNT(*) filter (WHERE "recommend")
+    ) AS recommended
     FROM reviews
     WHERE product = ${num}`
     , (err, data) => {
