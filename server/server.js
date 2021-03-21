260604789e617c870be661a871940ee22a6f62f8
Adam Whitman <emagdaeh@yahoo.com>
2021-03-20 21:12:39 -0700

    All endpoints checked with postman and Project Catwalk and are working with Artillery test at < 10ms for 60s at 10 rps

--- server/server.js
+++ server/server.js
@@ -11,42 +11,31 @@ const port = 3000;
 app.use(morgan('dev'));
 app.use(express.json());
 
-const reviewsResultsArrayBuilder = async (num, resultCount, sortBy, callback) => {
-  const product = num;
-  const sort = sortBy;
-  const resultsTotal = resultCount;
-
-  const resultsArr = `SELECT reviews.review_id, reviews.rating, reviews.summary, reviews.recommend, reviews.response, reviews.body, reviews.date, reviews.reviewer_name, reviews.helpfulness, (
-    SELECT JSON_BUILD_OBJECT(
+const reviewsResultsArrayBuilder = async (product, limit, sortBy, callback) => {
+  const resultsArr = `SELECT reviews.review_id, reviews.rating, reviews.summary, reviews.recommend, reviews.response, reviews.body, reviews.date, reviews.reviewer_name, reviews.helpfulness,
+    JSON_AGG(JSON_BUILD_OBJECT(
       'id', id,
       'url', url
-    ) AS photos
-    FROM reviews_photos
-    WHERE reviews_photos.review_id = $1
-    GROUP BY id)
+    )) AS photos
   FROM reviews
   LEFT JOIN reviews_photos
-  ON reviews_photos.review_id = reviews.review_id
+  ON (reviews_photos.review_id = reviews.review_id)
   WHERE reviews.product = $1 AND reviews.reported = false
   GROUP BY reviews.review_id
   ORDER BY $2
   LIMIT $3`;
 
-  let response;
+  const response = await database.query(resultsArr, [product, sortBy, limit]);
 
   try {
-    response = await database.query(resultsArr, [product, sort, resultsTotal]);
+    callback(null, response);
   } catch (err) {
     console.log(err.stack);
   }
-
-  callback(null, response);
 };
 
 app.get('/reviews/meta/:product_id?', async (req, res) => {
   const productId = req.query.product_id;
-
-  console.log('productId: ', productId);
 
   const recommendStr = `SELECT JSON_BUILD_OBJECT(
     'false', COUNT(*) filter (WHERE NOT "recommend"),
@@ -117,24 +106,11 @@ app.get('/reviews/meta/:product_id?', async (req, res) => {
   }
 });
 
-app.get('/reviews/:product_id:count?', (req, res) => {
-  let { page, count, sort } = req.params;
-  // const productId = req.params.product_id;
+app.get('/reviews/:product_id?', (req, res) => {
   const productId = req.query.product_id;
-
-console.log('req.params: ', req.params);
-
-  if (page === undefined) {
-    page = 0;
-  }
-
-  if (count === undefined) {
-    count = 5;
-  }
-
-  if (sort === undefined) {
-    sort = 'relevant';
-  }
+  const count = req.query.count || 5;
+  const sort = req.query.sort || 'helpful';
+  const page = req.query.page || 0;
 
   const sortingFunc = (str) => {
     if (str === 'helpful') {
@@ -165,83 +141,10 @@ console.log('req.params: ', req.params);
   });
 });
 
-// app.get('/reviews/meta/:product_id', async (req, res) => {
-//   const productId = req.query.product_id;
-
-//   console.log('productId: ', productId);
-
-//   const recommendStr = `SELECT JSON_BUILD_OBJECT(
-//     'false', COUNT(*) filter (WHERE NOT "recommend"),
-//     'true', COUNT(*) filter (WHERE "recommend")
-//   ) AS recommended
-//   FROM reviews
-//   WHERE product = $1`;
-
-//   const ratingString = `SELECT JSON_BUILD_OBJECT(
-//     '1', SUM(1),
-//     '2', SUM(2),
-//     '3', SUM(3),
-//     '4', SUM(4),
-//     '5', SUM(5)
-//   ) AS ratings
-//   FROM reviews
-//   WHERE product = $1`;
-
-//   const characterNamesString = 'SELECT name FROM characteristics WHERE characteristics.product_id = $1';
-
-//   const charUniqueIdString = 'SELECT id FROM characteristics WHERE characteristics.product_id = $1';
-
-//   const getCharIds = 'SELECT id FROM characteristics WHERE characteristics.product_id = $1';
-
-//   const recommendedObj = await database.query(recommendStr, [productId]);
-//   const ratingsObj = await database.query(ratingString, [productId]);
-//   const characteristicNames = await database.query(characterNamesString, [productId]);
-//   const characterisiticUniqueIds = await database.query(charUniqueIdString, [productId]);
-//   const charIdsAndValsQuery = await database.query(getCharIds, [productId]);
-
-//   let str = '(';
-
-//   for (let i = 0; i < charIdsAndValsQuery.rows.length; i++) {
-//     if (i === charIdsAndValsQuery.rows.length - 1) {
-//       str = str.concat(charIdsAndValsQuery.rows[i].id.toString(), ')');
-//     } else {
-//       str = str.concat(charIdsAndValsQuery.rows[i].id.toString(), ', ');
-//     }
-//   }
-
-//   const avgCalcString = `SELECT characteristic_id, AVG(value) AS "value" FROM characteristic_reviews WHERE characteristic_reviews.characteristic_id IN ${str} GROUP BY characteristic_id`;
-
-//   const avgs = await database.query(avgCalcString);
-
-//   const names = characteristicNames.rows;
-//   const ids = characterisiticUniqueIds.rows;
-//   const vals = avgs.rows;
-
-//   const characteristics = {};
-
-//   for (let i = 0; i < names.length; i++) {
-//     characteristics[names[i].name] = {};
-//     characteristics[names[i].name].id = ids[i].id;
-//     characteristics[names[i].name].value = vals[i].value;
-//   }
-
-//   const metaObj = {
-//     product_id: productId,
-//     recommended: recommendedObj.rows[0].recommended,
-//     ratings: ratingsObj.rows[0].ratings,
-//     characteristics,
-//   };
-
-//   try {
-//     res.send(metaObj);
-//   } catch (err) {
-//     res.sendStatus(500);
-//   }
-// });
-
 app.post('/reviews', async (req, res) => {
+  console.log('req.body: ', req.body);
   const {
-    productId,
+    product_id,
     rating,
     summary,
     body,
@@ -285,7 +188,7 @@ app.post('/reviews', async (req, res) => {
   });
 
   try {
-    await database.query(reviewStr, [productId, rating, formatDate, summary, body, recommend, defaultReported, name, email, defaultResponse, defaultHelpfulness]);
+    await database.query(reviewStr, [product_id, rating, formatDate, summary, body, recommend, defaultReported, name, email, defaultResponse, defaultHelpfulness]);
 
     await database.query(photosStr, [urlStr, reviewId]);
     res.sendStatus(201);
