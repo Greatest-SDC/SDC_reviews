86469e754050583bbedf2f7caec9bef49c5561e9
Adam Whitman <emagdaeh@yahoo.com>
2021-03-25 21:36:16 -0700

    Adding morgan back in

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
+const morgan = require('morgan')
 const database = require('../database');
 // const TOKEN = require('../config.js');
 
 const app = express();
 const port = 3000;
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static('public'));
 
